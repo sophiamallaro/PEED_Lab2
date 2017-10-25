9e65ac89fc26c19918fa3a404ba7052795fc642e
Sophia Mallaro <sophia-mallaro@uiowa.edu>
2017-10-25 16:49:41 -0500

    Update server.py

--- networking/server.py
+++ networking/server.py
@@ -1,12 +1,37 @@
-import socket
-
-UDP_IP = '0.0.0.0'  # 0.0.0.0 IPv4
-UDP_PORT = 5005
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-s.bind((UDP_IP, UDP_PORT))
-
-while True:
-    data, addr = s.recvfrom(1024)  # 1024 bytes buffer
-    print('received message', data)
+import pyaudio
+import wave
+import opuslib
+ 
+FORMAT = 8
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "file.wav"
+ 
+audio = pyaudio.PyAudio()
+ 
+# start Recording
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,
+                frames_per_buffer=CHUNK)
+print ("recording...");
+frames = []
+ 
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+print ("finished recording");
+print(frames.type);
+ 
+# stop Recording
+stream.stop_stream()
+stream.close()
+audio.terminate()
+ 
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
